cc51b61a91b9030930d7859979403c60f1326f29
Steven Lundy <steven.lundy@gmail.com>
2016-03-19 10:45:54 -0700

    Create helper function to add multiple classes

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
+var addClasses = function (el, ...classes) {
+  classes.forEach(className => el.classList.add(className));
+}
+
 var tools = document.querySelectorAll('.toolbar-commenting');
 for (var i = 0; i < tools.length; i++) {
   var toolgroup = document.createElement('div');
   toolgroup.classList.add('toolbar-group');
   var giphyButton = document.createElement('button');
   giphyButton.innerHTML = 'GIF';
-  giphyButton.classList.add('toolbar-item');
-  giphyButton.classList.add('tooltipped');
-  giphyButton.classList.add('tooltipped-nw');
+  addClasses(giphyButton, 'toolbar-item', 'tooltipped', 'tooltipped-nw');
   giphyButton.setAttribute('aria-label', 'Add a random giphy');
   toolgroup.appendChild(giphyButton);
   tools[i].appendChild(toolgroup);
