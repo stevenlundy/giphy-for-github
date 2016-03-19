f296c78472c8dda55c81ccf43d0eed46bfed7eb5
Steven Lundy <steven.lundy@gmail.com>
2016-03-19 10:39:22 -0700

    Add GIF button to toolbars

--- main.js
+++ main.js
@@ -1,4 +1,16 @@
 var tools = document.querySelectorAll('.toolbar-commenting');
 for (var i = 0; i < tools.length; i++) {
-  // Do something to manipulate each toolbar
+  var toolgroup = document.createElement('div');
+  toolgroup.classList.add('toolbar-group');
+  var giphyButton = document.createElement('button');
+  giphyButton.innerHTML = 'GIF';
+  giphyButton.classList.add('toolbar-item');
+  giphyButton.classList.add('tooltipped');
+  giphyButton.classList.add('tooltipped-nw');
+  giphyButton.setAttribute('aria-label', 'Add a random giphy');
+  toolgroup.appendChild(giphyButton);
+  tools[i].appendChild(toolgroup);
+  tools[i].addEventListener('click', function(e) {
+    e.preventDefault();
+  })
 }
